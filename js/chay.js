f6f4ba176a6227a5df94a87d5610cf6358dd13e3
huongnguyenthithuy2510 <huong.ntt697@arena-multimedia.edu.vn>
2024-10-06 15:15:14 -0200

    Add files via upload

---
+++ js/chay.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".auto-type", {
+    strings: ["Programmer", "Designer", "YouTuber"], // Các chuỗi sẽ được gõ
+    typeSpeed: 150,  // Tốc độ gõ (ms)
+    backSpeed: 150,   // Tốc độ xóa (ms)
+    loop: true        // Lặp lại hiệu ứng
+});
